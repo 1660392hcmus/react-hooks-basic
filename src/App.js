c7f4904a326b37c8461dd3ecb372f37981b7da8f
1660392hcmus <1660392@student.hcmus.edu.vn>
2020-04-07 17:37:40 +0700

    implement todolist using hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "todo oneeeeeee" },
+    { id: 2, title: "todo twooooooo" },
+    { id: 3, title: "todo threeeeee" }
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo.id);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>hello</h1>
       <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick}></TodoList>
     </div>
   );
 }
